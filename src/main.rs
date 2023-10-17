18eabe20cf7204e7131dc5b037aa8dd47b59e648
Luisander69 <32811852+Luisander69@users.noreply.github.com>
2023-10-17 08:31:19 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,31 @@ use std::process::{Command, exit,};                             use std::io{self
 
 
 
+fn executecmd(cmd: &str) -> String{
+    
+let res = if cfg!(target_os = "windows") {
+        let temp = "/C ".to_owned();
+        let fullcmd = temp + cmd;
+        let cmds: Vec<&str> = fullcmd.split(" ").collect();
+        Command::new("cmd").arg(&cmds[0]).arg(&cmds[1]).output().unwrap()
+} else {
+        let temp = "-c ".to_owned();
+        let fullcmd = temp + cmd;
+        let cmds: Vec<&str> = fullcmd.split(" ").collect();
+        Command::new("sh").arg(&cmds[0]).arg(&cmds[1]).output().unwrap()
+};
+
+        let stdout = String::from_utf8_lossy(res.stdout.as_slice());
+        let stderr = String::from_utf8_lossy(res.stderr.as_slice());
+
+        if stdout.len() > 0{
+            return stdout.to_string();
+        }else{
+            return stderr.to_string();
+        }
+
+}
+
 
 
 fn main() {
