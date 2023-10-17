f5f9b24588b535cf00e9ddb93ce2e2cc67ceb52c
luisander <stefaniukliubomyr@gmail.com>
2023-10-17 06:24:03 +0000

    initial commit. before adding commandcmd

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::net::{Ipv4Addr, SocketAddrV4, TcpListener, TcpStream, Shutdown};
+use std::process::{Command, exit,};                             use std::io{self, Write, BufReader};
+
+
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
